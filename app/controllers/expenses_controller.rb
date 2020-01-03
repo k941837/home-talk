423ed0f765bdcb5ebc27e5a5c10f9b0e97b1b5f4
k941837 <55436678+k941837@users.noreply.github.com>
2020-01-03 18:18:47 +0900

    月別管理

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -7,16 +7,23 @@ class ExpensesController < ApplicationController
   end
 
   def index
+    @expense = Expense.new
+    @expenses = Expense.all
     @user = current_user
+    d = Date.today;
+    @month_expenses = @user.expenses.where(year_id:2, month_id:d.month )
+    @month_incomes = @user.incomes.where(year_id:2, month_id:d.month )
+
     @chart = {}
     unless @user.expenses[0].blank?
-      @user.expenses.each do |b|
+      @month_expenses.each do |b|
         # @chart = { @user.expenses[0].expensecategory.name => @user.expenses[0].money , @user.expenses[1].expensecategory.name => @user.expenses[1].money }
         @chart.store(b.expensecategory.name, b.money)
       end
     end
   end
   
+    
 
   def new
     @expense = Expense.new
@@ -26,6 +33,27 @@ class ExpensesController < ApplicationController
     Expense.create(expense_params)
     redirect_to expenses_path
   end
+
+
+  def search_expense
+
+    @expenses = Expense.all
+    @user = current_user
+    @month_expenses = @user.expenses.where(year_id:params[:year_id], month_id:params[:month_id])
+    @month_incomes = @user.incomes.where(year_id:params[:year_id], month_id:params[:month_id])
+
+    @chart = {}
+    unless @user.expenses[0].blank?
+      @month_expenses.each do |b|
+        # @chart = { @user.expenses[0].expensecategory.name => @user.expenses[0].money , @user.expenses[1].expensecategory.name => @user.expenses[1].money }
+        @chart.store(b.expensecategory.name, b.money)
+      end
+    end
+    @ppp = "#{Month.find(params[:month_id]).name}の合計収入は"
+    @qqq = "#{Month.find(params[:month_id]).name}の合計支出は"
+
+  end
+  
 
 
   def expense_params
